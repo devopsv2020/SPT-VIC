cd1874810ae6c5c21e29a47bef3831947ed60897
devopsv2020 <33793351+devopsv2020@users.noreply.github.com>
2021-08-03 15:48:05 -0400

    Create BuildConfig.groovy

---
+++ src/com/devops2020/data/BuildConfig.groovy
@@ -0,0 +1,15 @@
+package com.devops2020.stats.data
+
+/**
+*Document 
+*/
+
+class BuildConfig {
+  
+  final static String VIC_LIBRARY = 'vic-jenkins-shared-library'
+  final static string VIC_LIBRARY_VERSION = 'latest'
+  final static Map WEBEX_CHANNELS = [ 
+    build: 'add-string-here'
+    deploy: 'add-strings-here'
+    ]
+}
